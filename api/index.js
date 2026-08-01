eb00610e9261983b1e4c20adaa62ad3981e73650
Glysdi Jahzeel Loplop <lglysdijahzeel@gmail.com>
2026-08-01 23:45:31 +0800

    Fix Vercel routing

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const userRoutes = require("../routes/users");
 
@@ -8,10 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
-
 app.get("/", (req, res) => {
-    res.json({ message: "Backend API is running!" });
+  res.json({
+    message: "Backend API is running!"
+  });
 });
+
+app.use("/api/users", userRoutes);
 
 module.exports = app;
